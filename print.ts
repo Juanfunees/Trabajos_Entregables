c4ace7d84efa381aa920a1a456a0a5da4952899e
juanfunees <juan.funees@gmail.com>
2024-09-29 18:18:36 -0300

    Distintas letras

---
+++ print.ts
@@ -0,0 +1,13 @@
+/*
+
+                EJERCICIO 1
+
+1) Crear un arreglo de letras e imprimirlo 
+
+*/
+
+// se crea un arreglo llamado LETRAS donde contiene distintas letras
+// se muestra por pantalla el arreglo
+
+let letras: string[] = new Array("A,B,C,D,E,F,G");
+console.log("El arreglo contiene las letras:", letras);
